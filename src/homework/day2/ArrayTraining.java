c6107ae506288ed8caef6064741f2dca9f8146f0
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-09 22:14:56 +0300

    add return reverse array with elements larger than n method

--- src/homework/day2/ArrayTraining.java
+++ src/homework/day2/ArrayTraining.java
@@ -1,10 +1,13 @@
 package homework.day2;
 
+import java.util.Arrays;
+
 public class ArrayTraining {
     public static void main(String[] args) {
         int[] newArray = {2,4,5,3,1,6};
         ArrayTraining arrayTraining = new ArrayTraining();
         System.out.println(arrayTraining.sumNElementsArray(newArray, 3));
+        System.out.println(Arrays.toString(arrayTraining.returnReverseArrayLargerN(newArray, 3)));
     }
 
     public int sumNElementsArray(int[] array, int n) {
@@ -19,5 +22,35 @@ public class ArrayTraining {
         }
 
         return sum;
+    }
+
+    public int[] returnReverseArrayLargerN(int[] array, int n) {
+        int counter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > n) {
+                counter++;
+            }
+        }
+
+        int[] secondArray = new int[counter];
+        int newCounter = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > n) {
+                secondArray[newCounter] = array[i];
+                newCounter++;
+            }
+        }
+
+        int[] thirdArray = new int[secondArray.length];
+        int anotherCounter = 0;
+
+        for (int i = secondArray.length - 1; i >= 0; i--) {
+            thirdArray[anotherCounter] = secondArray[i];
+            anotherCounter++;
+        }
+
+        return thirdArray;
     }
 }
