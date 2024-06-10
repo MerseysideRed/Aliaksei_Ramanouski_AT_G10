aee58b0a7069190aaeebfb6458ab04046cfa62f1
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-10 12:07:28 +0300

    add returnSumSpecificElements method and change elements of new aaray for tests

--- src/homework/day2/ArrayTraining.java
+++ src/homework/day2/ArrayTraining.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class ArrayTraining {
     public static void main(String[] args) {
-        int[] newArray = {2,4,5,3,1,6};
+        int[] newArray = {12, 4, 5, 3, 7, 6, 1, 8, 9, 1, 2, 1, 12, 4, 5, 3, 7, 6, 1, 8, 9, 1, 2, 1};
         ArrayTraining arrayTraining = new ArrayTraining();
         System.out.println(arrayTraining.sumNElementsArray(newArray, 3));
         System.out.println(Arrays.toString(arrayTraining.returnReverseArrayLargerN(newArray, 3)));
+        System.out.println(arrayTraining.returnSumSpecificElements(newArray));
     }
 
     public int sumNElementsArray(int[] array, int n) {
@@ -49,5 +50,31 @@ public class ArrayTraining {
         }
 
         return thirdArray;
+    }
+
+    public int returnSumSpecificElements(int[] array) {
+        int firstNumber = 0;
+        if (array.length < 10) {
+            firstNumber = array.length;
+        } else if (array.length < 100) {
+            firstNumber = array.length / 10;
+        } else if (array.length < 1000) {
+            firstNumber = array.length / 100;
+        } else if (array.length < 10000) {
+            firstNumber = array.length / 1000;
+        } else if (array.length < 100000) {
+            firstNumber = array.length / 10000;
+        } else if (array.length < 1000000) {
+            firstNumber = array.length / 100000;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % firstNumber == 0) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
     }
 }
