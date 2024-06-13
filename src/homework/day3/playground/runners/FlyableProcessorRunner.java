e9759f2f7b72bce420746d54dd37db4065c9e0c3
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-13 17:01:56 +0300

    completed task 4 and added another constructor for Plant class without name field

---
+++ src/homework/day3/playground/runners/FlyableProcessorRunner.java
@@ -0,0 +1,52 @@
+package homework.day3.playground.runners;
+
+import homework.day3.playground.essence.Flyable;
+import homework.day3.playground.essence.craft.air.Aircraft;
+import homework.day3.playground.essence.craft.air.Copter;
+import homework.day3.playground.essence.craft.air.Plane;
+import homework.day3.playground.essence.craft.air.Rocket;
+import homework.day3.playground.essence.creatures.Fly;
+import homework.day3.playground.essence.creatures.Insect;
+import homework.day3.playground.essence.creatures.Mosquito;
+import homework.day3.playground.processors.FlyableProcessor;
+
+public class FlyableProcessorRunner {
+    public static void main(String[] args) {
+        FlyableProcessor flyableProcessor = new FlyableProcessor();
+
+        Flyable flyableCopter = new Copter(223, "Mi8 Flyable");
+        Flyable flyablePlane = new Plane(3452, "Boeing 837 Flyable");
+        Flyable flyableRocket = new Rocket(7623, "Super Heavy Flyable");
+        Flyable flyableFly = new Fly(23, "Domestica Flyable");
+        Flyable flyableMosquito = new Mosquito(12, "Vulgaris Flyable");
+        Aircraft aircraftCopter = new Copter(223, "Mi8 Aircraft");
+        Aircraft aircraftPlane = new Plane(3452, "Boeing 837 Aircraft");
+        Aircraft aircraftRocket = new Rocket(7623, "Super Heavy Aircraft");
+        Insect insectFly = new Fly(23, "Domestica Insect");
+        Insect insectMosquito = new Mosquito(12, "Vulgaris Insect");
+        Copter aCopter = new Copter(223, "Mi8 Copter");
+        Plane aPlane = new Plane(3452, "Boeing 837 Plane");
+        Rocket aRocket = new Rocket(7623, "Super Heavy Rocket");
+        Fly aFly = new Fly(23, "Domestica Fly");
+        Mosquito aMosquito = new Mosquito(12, "Vulgaris Mosquito");
+
+        flyableProcessor.runFlyable(flyableCopter);
+        flyableProcessor.runFlyable(flyablePlane);
+        flyableProcessor.runFlyable(flyableRocket);
+        flyableProcessor.runFlyable(flyableFly);
+        flyableProcessor.runFlyable(flyableMosquito);
+        flyableProcessor.runFlyable(aircraftCopter);
+        flyableProcessor.runFlyable(aircraftPlane);
+        flyableProcessor.runFlyable(aircraftRocket);
+        //flyableProcessor.runFlyable(insectFly); // There is a compilation error because Insect class doesn't implement Flyable interface. We can transfer to method from interface only instances from related classes for this interface
+        //flyableProcessor.runFlyable(insectMosquito); //There is a compilation error because Insect class doesn't implement Flyable interface. We can transfer to method from interface only instances from related classes for this interface
+        flyableProcessor.runFlyable(aCopter);
+        flyableProcessor.runFlyable(aPlane);
+        flyableProcessor.runFlyable(aRocket);
+        flyableProcessor.runFlyable(aFly);
+        flyableProcessor.runFlyable(aMosquito);
+
+        flyableProcessor.runFlyable(aFly, "никуда");
+        flyableProcessor.runFlyable(flyableCopter, "повсюду");
+    }
+}
