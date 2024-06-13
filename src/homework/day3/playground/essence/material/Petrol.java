355771105d07f73c7e8c2a0be990f40d43c1c220
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-13 12:00:18 +0300

    add interfaces and their implementation for appropriate classes

--- src/homework/day3/playground/essence/material/Petrol.java
+++ src/homework/day3/playground/essence/material/Petrol.java
@@ -2,7 +2,7 @@ package homework.day3.playground.essence.material;
 
 import homework.day3.playground.essence.Matter;
 
-public class Petrol extends Matter {
+public class Petrol extends Matter implements Pourable, Powerable {
     public Petrol(int mass) {
         super(mass);
     }
--- src/homework/day3/playground/essence/material/Water.java
+++ src/homework/day3/playground/essence/material/Water.java
@@ -2,7 +2,7 @@ package homework.day3.playground.essence.material;
 
 import homework.day3.playground.essence.Matter;
 
-public class Water extends Matter {
+public class Water extends Matter implements Pourable {
     public Water(int mass) {
         super(mass);
     }
