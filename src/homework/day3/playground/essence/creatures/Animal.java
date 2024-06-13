6d85bfe1e93e549867a1d6bb7db441e3bf1d703a
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-13 05:54:07 +0300

    add getters and setters for field name for Animal, Aircraft, Vehicle, Container classes and adjust imports for previous hometask

--- src/homework/day3/playground/essence/creatures/Animal.java
+++ src/homework/day3/playground/essence/creatures/Animal.java
@@ -9,4 +9,12 @@ public abstract class Animal extends Matter {
         super(mass);
         this.name = name;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
