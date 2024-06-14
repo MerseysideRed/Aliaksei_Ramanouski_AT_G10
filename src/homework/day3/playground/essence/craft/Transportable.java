f0bd6315e324d9e834e46f54bc2e04393d5a471d
Aliaksei Ramanouski <a.romanovskiy94@gmail.com>
2024-06-14 05:40:35 +0300

    completed task 5

--- src/homework/day3/playground/essence/craft/Transportable.java
+++ src/homework/day3/playground/essence/craft/Transportable.java
@@ -1,4 +1,11 @@
 package homework.day3.playground.essence.craft;
 
 public interface Transportable {
+    default int move(int pointA, int pointB) {
+        System.out.printf("I am %s, my name is %s and I am moving from point %d to point %d\n", this.getClass().getSimpleName(), getName(), pointA, pointB);
+
+        return pointB - pointA;
+    }
+
+    String getName();
 }
